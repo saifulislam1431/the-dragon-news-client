3bde593f93666ce739f1d4c6c6dd46c19ee0a958
Saiful Islam <sakibbhaiya143@gmail.com>
2023-04-19 18:58:30 +0600

    Header component added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../index.css'
 import Header from '../pages/Shared/Header/Header';
 import Footer from '../pages/Shared/Footer/Footer';
 import { Col, Container, Row } from 'react-bootstrap';
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,13 +1,56 @@
 import React from 'react';
+import '../../../index.css'
 import logo from '../../../assets/logo.png';
 import moment from 'moment/moment';
+import Marquee from "react-fast-marquee";
+import profile from '../../../assets/profile-user.png'
+import { Link, NavLink } from 'react-router-dom';
 const Header = () => {
     return (
-        <header>
+        <header className='d-flex py-3'>
+            <div>
             <div className="top-nav text-center pt-4">
-             <img src={logo} alt="" className='img-fluid'/>
-               <p className='text-secondary fw-semibold pt-2'>Journalism Without Fear or Favour</p>
-               <p className='text-secondary'><span className='fw-bold'>{moment().format('dddd')},</span> <span className='fw-semibold'>{moment().format("MMMM D, YYYY")}</span></p>
+                <img src={logo} alt="" className='img-fluid' />
+                <p className='text-secondary fw-semibold pt-2'>Journalism Without Fear or Favour</p>
+                <p className='text-secondary'><span className='fw-bold'>{moment().format('dddd')},</span> <span className='fw-semibold'>{moment().format("MMMM D, YYYY")}</span></p>
+            </div>
+            <div className="latest-news my-3 d-inline-flex align-items-center bg-secondary bg-opacity-10 p-3">
+                <div>
+                    <button className='btn btn-danger fw-semibold rounded-1 px-4'>Latest</button>
+                </div>
+                <div>
+                    <Marquee gradientColor={0} pauseOnHover={true} className='fw-semibold'>
+                        Match Highlights: Germany vs Spain — as it happened   !
+                        <span className='mx-5'>
+                            I can be a React component, multiple React components, or just some text.
+                        </span>
+                        <span>
+                            Match Highlights: Germany vs Spain as...
+                        </span>
+                    </Marquee>
+                </div>
+
+            </div>
+            <nav className='my-3 d-flex align-items-center justify-content-end'>
+                <div className='me-5'>
+                    <div>
+                        <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'default')}>Home</NavLink>
+                        <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : 'default')}>About</NavLink>
+                        <NavLink to="/career" className={({ isActive }) => (isActive ? 'active' : 'default')}>Career</NavLink>
+                    </div>
+                </div>
+                <div>
+                    <div className='d-flex align-items-center gap-1'>
+                        <div>
+                            <img src={profile} alt="" className='w-75'/>
+                        </div>
+                        <div>
+                            <button className='btn btn-dark rounded-0 px-4 py-1'>Login</button>
+                        </div>
+                    </div>
+                </div>
+
+            </nav>
             </div>
         </header>
     );
