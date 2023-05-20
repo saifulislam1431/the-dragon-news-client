e05578ae1ea5c815dec8cbb75d9bd08a515741f0
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-20 22:12:03 +0600

    Footer added

--- src/component/category/Category.jsx
+++ src/component/category/Category.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 import SingleNews from '../../pages/SingleNews/SingleNews';
 
 const Category = () => {
+    const[showAll , setShowAll] = useState(false);
     const {id} = useParams();
     const categoriesNews = useLoaderData();
     return (
         <div>
             <h5 className='fw-bold mb-5 text-center'>Dragon News</h5>
             {
-                categoriesNews.map(news => <SingleNews
+                categoriesNews.slice(0,showAll ? 10 : 3).map(news => <SingleNews
                 key={news._id}
                 news={news}
                 ></SingleNews>)
+            }
+            {
+                !showAll && <div className="my-4 text-center">
+                <button className='px-2 py-1 bg-danger text-white border-danger rounded-1' onClick={()=>setShowAll(!showAll)}>Show More</button>
+
+            </div>
             }
         </div>
     );
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import logo from "../../../assets/logo.png"
 
 const Footer = () => {
     return (
-        <div>
-            <p className='text-center'>Footer Coming Soon...!</p>
-        </div>
+        <footer className="d-flex flex-column gap-5 flex-lg-row p-5 bg-light text-base-content mt-5">
+  <div>
+    <img src={logo} alt="Logo" className='w-75 mb-2'/>
+    <p><span className='fw-semibold'>The Dragon News.</span><br/>Providing trusted news since 2023</p>
+    <p>Copyright © 2023 - All right reserved by Dragon News</p>
+  </div> 
+  <div className='d-grid'>
+    <span className="fw-bold text-black-50">Services</span> 
+    <a className=" text-black text-decoration-none">Branding</a> 
+    <a className=" text-black text-decoration-none">Design</a> 
+    <a className=" text-black text-decoration-none">Marketing</a> 
+    <a className=" text-black text-decoration-none">Advertisement</a>
+  </div> 
+  <div className='d-grid'>
+    <span className="fw-bold text-black-50">Company</span> 
+    <a className="text-black text-decoration-none">About us</a> 
+    <a className="text-black text-decoration-none">Contact</a> 
+    <a className="text-black text-decoration-none">Jobs</a> 
+    <a className="text-black text-decoration-none">Press kit</a>
+  </div> 
+  <div className='d-grid'>
+    <span className="fw-bold text-black-50">Legal</span> 
+    <a className="text-black text-decoration-none">Terms of use</a> 
+    <a className="text-black text-decoration-none">Privacy policy</a> 
+    <a className="text-black text-decoration-none">Cookie policy</a>
+  </div>
+</footer>
     );
 };
 
